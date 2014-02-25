e3059125016ce80672c19494750a8895ddd2b1b0
Gijs Molenaar <gijs@pythonic.nl>
2014-02-25 12:21:40 +0200

    small changes

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ docker build -t gijzelaerr/discoservice build/service
 docker build -t gijzelaerr/discotest build/test
 
 # make sure the old one is gone
-docker rm discoservice
+docker stop discoservice || true
+docker rm discoservice || true
+
 docker run -d -p 2222:22 -p 8989:8989 -name discoservice -t -i gijzelaerr/discoservice
-docker run -t -i --link=discoservice:discoservice gijzelaerr/discotest 
+#docker run -t -i --link=discoservice:discoservice gijzelaerr/discotest 
