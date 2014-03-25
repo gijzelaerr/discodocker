c0cbb1378e17cc3eabf5b53174c890cf1f9f1a60
Gijs Molenaar <gijs@pythonic.nl>
2014-03-25 13:27:04 +0200

    small changes

--- build/base/scripts/discostable.sh
+++ build/base/scripts/discostable.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/discoproject/disco.git /disco
 cd /disco
+git checkout tags/0.5
 make
 make install
 
--- run.sh
+++ run.sh
@@ -8,5 +8,15 @@ docker build -t gijzelaerr/discotest build/test
 docker stop discoservice || true
 docker rm discoservice || true
 
-docker run -d -p 2222:22 -p 8989:8989 -name discoservice -t -i gijzelaerr/discoservice
+docker run \
+    -d \
+    -t \
+    -p 2222:22 \
+    -p 8989:8989 \
+    -name discoservice \
+    gijzelaerr/discoservice
+
+docker port discoservice 22
+docker port discoservice 8989
+
 #docker run -t -i --link=discoservice:discoservice gijzelaerr/discotest 
