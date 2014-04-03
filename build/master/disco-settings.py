086218dca3ad2bb60f44ab294cadd0750de4e99d
Gijs Molenaar <gijs@pythonic.nl>
2014-04-03 13:11:04 +0200

    add disco settings

---
+++ build/master/disco-settings.py
@@ -0,0 +1,10 @@
+DISCO_HOME = "/usr/local/lib/disco"
+DISCO_ROOT = "/usr/local/var/disco"
+DISCO_WWW_ROOT = "/usr/local/share/disco/master/www"
+DISCO_PORT = 8989
+DISCO_PROXY_ENABLED = "on"
+# DISCO_HTTPD = "/usr/sbin/varnishd -a 0.0.0.0:$DISCO_PROXY_PORT -f $DISCO_PROXY_CONFIG -P $DISCO_PROXY_PID -n/tmp -smalloc"
+
+DDFS_TAG_MIN_REPLICAS = 3
+DDFS_TAG_REPLICAS     = 3
+DDFS_BLOB_REPLICAS    = 3
--- build/test/test.sh
+++ build/test/test.sh
@@ -1,4 +1,5 @@
 export DISCO_MASTER_HOST=${DISCOMASTER_PORT_8989_TCP_ADDR}
-export DISCO_PROXY=${DISCOMASTER_PORT_8999_TCP_ADDR}
+export DISCO_PROXY=http://${DISCOMASTER_PORT_8999_TCP_ADDR}:8999
+
 env
 su -c "python /disco/examples/util/count_words.py" disco
